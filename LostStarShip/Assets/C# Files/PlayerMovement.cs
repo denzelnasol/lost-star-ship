8a00a5c9105d5b5667b3e353449c997510231f56
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-09-24 14:03:28 -0700

    Add player shooting, bullets, and explosion effects

---
+++ LostStarShip/Assets/C# Files/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    float leftMove = 0f;
+    float rightMove = 0f;
+
+    public Animator animator;
+    public Rigidbody2D rb;
+
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        leftMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(leftMove));
+
+        rightMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        animator.SetFloat("Speed", rightMove);
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
